eb9eca8846c1c52ec85046b9f28dc47e9f50fd83
qaws7791 <qaws7791@gmail.com>
2024-04-24 10:08:44 +0900

    Feat: 존재하지 않은 경로에 대한 오류 응답 추가

--- src/app.ts
+++ src/app.ts
@@ -5,6 +5,7 @@ import { config } from "dotenv";
 import { errorMiddleware } from "./middleware/error.middleware";
 import { contentTypeMiddleware } from "./middleware/contentType.middleware";
 import { acceptMiddleware } from "./middleware/accept.middleware";
+import { AppError } from "./utils/appError";
 
 config();
 
@@ -18,6 +19,17 @@ app.use(acceptMiddleware);
 
 app.use("/api/v1/tasks", tasksRouter);
 app.use("/api/v1/auth", authRouter);
+
+// case of route not found
+app.use((req, res, next) => {
+  return next(
+    new AppError(
+      "Route not found",
+      404,
+      "The requested resource could not be found"
+    )
+  );
+});
 
 app.use(errorMiddleware);
 
