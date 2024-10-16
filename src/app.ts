efffb1e9137180e8ca314e83e92c53eeb0cb8ea8
qaws7791 <qaws7791@gmail.com>
2024-10-16 23:46:31 +0900

    Feat: 서버 설정 업데이트
    cors 추가, 4000번 포트로 변경

--- src/app.ts
+++ src/app.ts
@@ -3,22 +3,32 @@ import { config } from "dotenv";
 // Routes
 import tasksRouter from "./routes/tasks.route";
 import authRouter from "./routes/auth.route";
-import notFountRouter from "./routes/notFount.route";
+import notFoundRouter from "./routes/notFound.route";
 // Middleware
 import { errorMiddleware } from "./middleware/error.middleware";
 import { contentTypeMiddleware } from "./middleware/contentType.middleware";
 import { acceptMiddleware } from "./middleware/accept.middleware";
+import cors from "cors";
+import cookieParser from "cookie-parser";
 
 config();
 
 const app = express();
-const PORT = 3000;
+const PORT = 4000;
 
 app.use(
   express.json({
     type: ["json", "+json"],
   })
 );
+
+app.use(
+  cors({
+    origin: ["http://localhost:3000", "http://localhost:4000"],
+    credentials: true,
+  })
+);
+app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
 app.use(contentTypeMiddleware);
 app.use(acceptMiddleware);
@@ -27,7 +37,7 @@ app.use("/api/v1/tasks", tasksRouter);
 app.use("/api/v1/auth", authRouter);
 
 // case of route not found
-app.use(notFountRouter);
+app.use(notFoundRouter);
 
 app.use(errorMiddleware);
 
