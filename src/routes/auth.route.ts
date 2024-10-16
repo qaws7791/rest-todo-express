7087202a413bd2a9d0084117e61ac62c03149e7a
qaws7791 <qaws7791@gmail.com>
2024-10-16 23:45:17 +0900

    Feat: auth 경로에 reissue-token, status 엔드포인트 추가

--- src/routes/auth.route.ts
+++ src/routes/auth.route.ts
@@ -2,6 +2,7 @@ import express from "express";
 import AuthController from "../controllers/auth.controller";
 import { validateSchema } from "../middleware/validateSchema.middleware";
 import { LoginSchema, RegisterSchema } from "../schemas/auth.schema";
+import { authMiddleware } from "../middleware/auth.middleware";
 
 const router = express.Router();
 const authController = new AuthController();
@@ -12,5 +13,9 @@ router.post(
   authController.register
 );
 router.post("/login", validateSchema(LoginSchema), authController.login);
+
+router.post("/reissue-token", authController.renewAccessToken);
+
+router.post("/status", authMiddleware, authController.status);
 
 export default router;
